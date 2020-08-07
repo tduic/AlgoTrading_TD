871d98d0fe88a72aeab11ee0a0aff00c66714256
tyler <tduic@andrew.cmu.edu>
2020-08-07 00:29:24 -0400

    retrieving implied volatility by accessing ToS using selenium

--- utils/helpers.py
+++ utils/helpers.py
@@ -59,3 +59,61 @@ def historicalVolatility(t, symbol, n):
     stdDevAnnualized = stdDev * math.sqrt(252)
 
     return stdDevAnnualized
+
+def get_row_count(table):
+    return len(table.find_elements_by_tag_name("tr"));
+
+def get_column_count(table):
+    return len(table.find_elements_by_xpath("//tr[2]/td"));
+
+def impliedVolatility():
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    import pickle
+    with webdriver.Chrome(ChromeDriverManager().install()) as driver:
+        data = pickle.load(open(".login", "rb"))
+
+        # login to ToS web
+        driver.get('https://trade.thinkorswim.com/')
+        driver.find_element_by_id('username0').send_keys(data['username'])
+        driver.find_element_by_id('password').send_keys(data['password'])
+        driver.find_element_by_id('accept').click()
+        driver.implicitly_wait(10000)
+        driver.find_element_by_css_selector('details > summary').click()
+        driver.find_element_by_id('stepup_secretquestion0').click()
+        question = driver.find_elements_by_css_selector('main > div')[1].find_elements_by_css_selector('p')[1].text[10:]
+        driver.find_element_by_id('secretquestion0').send_keys(data[question])
+        driver.find_element_by_id('accept').click()
+        driver.find_element_by_css_selector('[for=trustthisdevice0_0]').click()
+        driver.find_element_by_id('accept').click()
+
+        # switch watchlist to AlgoList and add IV Percentile
+        watchlist = driver.find_element_by_css_selector('.watchlist-description')
+        watchlist.click()
+        driver.find_element_by_css_selector('[aria-label=AlgoList').click()
+        driver.find_element_by_css_selector('[aria-label="Column Configuration Collapsed"]').click()
+        driver.find_element_by_css_selector('[data-rbd-draggable-id="watchlist-table-table-column-drawer-IV Percentile"]').click()
+        driver.find_element_by_css_selector('[aria-label="Column Configuration Expanded"]').click()
+
+        # obtain IV and IV Percentile indices
+        headers = driver.find_element_by_css_selector('[data-rbd-droppable-id="watchlist-table-table-header"]')
+        row = headers.find_elements_by_xpath("//tr["+str(1)+"]/td")
+        ivIndex = 0
+        ivpIndex = 0
+        for i in range(len(row)) :
+            if row[i].text == 'IV':
+                ivIndex = i+1
+            if row[i].text == 'IV Pctl':
+                ivpIndex = i+1
+
+        # get IV and IVP for each asset
+        body = driver.find_element_by_id('watchlist-table-body')
+        rowCount = get_row_count(body)
+        colCount = get_column_count(body)
+        print(rowCount, colCount)
+        ivCol = body.find_elements_by_xpath("//tr/td["+str(ivIndex+1)+"]")
+        ivList = []
+        for elem in ivCol:
+            ivList.append(elem.text)
+        print(ivList)
+        time.sleep(10)
